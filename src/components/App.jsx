b6a2543b23816aeca67e58f0d7f7008fec5ec5fd
MazkaAndrii <andriy.mazka@gmail.com>
2023-02-04 19:50:05 +0200

    add Form Label Input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import React, { Component } from 'react';
+import Form from './Phonebook/Form/Form';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Form />
     </div>
   );
 };
